19d1af69f4748e333d42a9a6eee66e6581c3abe7
GoodPart <40373934+GoodPart@users.noreply.github.com>
2020-11-17 17:19:25 +0900

    푸시푸시

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -20,13 +20,16 @@ const cardSwiperFull = new Swiper('.swiper-container__card--full', {
     prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+    el: '.swiper-progress',
+    type: 'progressbar',
   },
 })
 
-/*------------------------------------*/
-const swiper2 = new Swiper('.swiper2', {
+const cardSwiper3 = new Swiper('.swiper-container__card--3', {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 121,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -35,35 +38,20 @@ const swiper2 = new Swiper('.swiper2', {
     el: '.swiper-pagination',
     clickable: true,
   },
-});
-
-
-const verticalSwiper = new Swiper('.vertical-swiper', {
-  direction: "vertical",
-  slidesPerView: 3,
-  // height: 49,
-  spaceBetween: 18,
-  slidesOffsetBefore: 10,
-  // slidesOffsetBefore: 39,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-
-const freeSwiper = new Swiper('.free-swiper', {
-    spaceBetween: 30,
-    freeMode: true,
-    slidesPerView: 'auto'
-
 })
-
 
 /////
 
+const header = document.querySelector(".header");
+const createDiv = document.createElement("div");
+
+createDiv.className="nav_bg";
+header.appendChild(createDiv);
+
 const headerList = document.querySelector(".header_list");
 const headerListItem = headerList.querySelectorAll(".header_list__list--item");
+
+const navBg = document.querySelector(".nav_bg");
 
 headerListItem.forEach(menu=> {
   const createSpan = document.createElement("span");
@@ -73,17 +61,62 @@ headerListItem.forEach(menu=> {
   menu.addEventListener("mouseover", (e)=> {
     if(menu.childNodes[1].classList.contains('line')) {
       menu.classList.remove("on")
+      navBg.classList.remove("on")
     }else {
       menu.classList.add("on")
+      navBg.classList.add("on")
     }
   })
 
   menu.addEventListener("mouseout", (e)=> {
       menu.classList.remove("on")
+      navBg.classList.remove("on")
   })
 })
 
-const header = document.querySelector(".header");
-const createDiv = document.createElement("div");
-createDiv.className="nav_bg";
-header.appendChild(createDiv);
+
+
+//strategy
+const leftTargetParent = document.querySelector(".section__body--article--left");
+const leftTargets = leftTargetParent.querySelectorAll(".section__list--item");
+// console.log("target, ", target)
+const rightTargetParent = document.querySelector(".section__body--article--right");
+const rightTargets = rightTargetParent.querySelectorAll(".section__list--item");
+
+leftTargets.forEach((leftTarget, index)=> {
+  const createBar = document.createElement("p");
+  createBar.className = 'check_bar';
+
+  leftTarget.appendChild(createBar);
+
+  //인텍스 삽입
+  leftTarget.setAttribute("data-index", index)
+
+  //좌측 탭 클릭시 이벤트 실행
+  leftTarget.addEventListener("click", (e)=> {
+    const checkBars = document.querySelectorAll(".check_bar");
+
+    //타겟에 data-index값을 저장함
+    const indexData =  e.path[1].attributes[1].value;
+    const originalClassName = "sprite-icon sprite-icon-check02"
+    const replacClassName = "sprite-icon sprite-icon-check02_active"
+
+    if(e.path[1].classList.contains("on")) {
+      console.log("on되어있다.")
+    }else {
+      for(var i = 0; i<leftTargets.length; i++) {
+        leftTargets[i].classList.remove("on");
+        rightTargets[i].classList.remove("on");
+        leftTargets[i].children[0].className = originalClassName
+        checkBars[i].classList.remove("on")
+        // e.path[1].children[0].className = originalClassName;
+      }
+      e.path[1].classList.add("on")
+      rightTargets[indexData].classList.add("on")
+      e.path[1].children[0].className = replacClassName;
+      checkBars[indexData].classList.add("on")
+      
+    }
+  })
+})
+
